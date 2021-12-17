711838fcb03c37eaf688a27a1363b9aa223fa0d1
barcelonaw <79105522+barcelonaw@users.noreply.github.com>
2021-12-17 07:27:18 -0800

    Add files via upload

---
+++ lab1/lab.c
@@ -0,0 +1,21 @@
+/*----------------*/
+/* Mico Barcelona*/
+/* Lab 1        */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main (void)
+{
+	printf("\nLab 1. \n\n");
+	printf("Hello World.\n");
+	printf("Hi, Mico Barcelona. \n\n");
+	printf("Once you know what failure feels like, determination chases success. -Kobe Bryant \n\n");
+
+	return Exit_SUCCESS;
+}
+/*----------------*/
+
+
+ 
